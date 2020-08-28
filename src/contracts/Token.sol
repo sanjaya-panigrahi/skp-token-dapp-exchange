3d8fb62daf9f53aab7efb2d21b2d09ca229f2ab5
sanjaya-panigrahi <info@sanjaya-panigrahi.tech>
2020-08-28 17:01:31 +1000

    ERC 20 Standard - Transfer event, Approval event and transferFrom Test cases implemented

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -9,26 +9,44 @@ contract Token {
     string public symbol = "SKP";
     uint256 public decimals = 18;
     uint256 public totalSupply;
-
-
-    // Track Balances
+    
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(address from, address to, uint256 value);
-    constructor() public{
-        totalSupply =  1000000 * (10 ** decimals);
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
 
-   
-    // Send tokens
-    function transfer(address _to, uint256 _value) public returns( bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
 
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        balanceOf[_from] = balanceOf[_from].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+    }
 
-        emit Transfer(msg.sender, _to, _value);
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
         return true;
     }
 }
